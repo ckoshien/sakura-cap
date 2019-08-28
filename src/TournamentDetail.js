8c82e672b960c7bf69f2c66db0d0d2253ee9036c
ckoshien <ckoshien@gmail.com>
2019-08-28 12:57:24 +0900

    ・アンカーリンクの色を桜色に ・太字の色を桜色に

--- src/TournamentDetail.js
+++ src/TournamentDetail.js
@@ -17,43 +17,43 @@ export default class TournamentDetail extends React.Component{
                   <td rowSpan={7}>1回戦</td>
                   <td>第1試合</td>
                   <td>明治</td>
-                  <td>3 - 1</td>
+                  <td><b>3</b> - 1</td>
                   <td>神奈総</td>
                 </tr>
                 <tr>
                   <td>第2試合</td>
                   <td>世田谷</td>
-                  <td><a href={'/tournament/first/2'}>1 - 2</a></td>
+                  <td><a href={'/tournament/first/2'}>1 - <b>2</b></a></td>
                   <td>横国B</td>
                 </tr>
                 <tr>
                   <td>第3試合</td>
                   <td>蓋世</td>
-                  <td><a href={'/tournament/first/3'}>10 - 2</a></td>
+                  <td><a href={'/tournament/first/3'}><b>10</b> - 2</a></td>
                   <td>蓋鯱</td>
                 </tr>
                 <tr>
                   <td>第4試合</td>
                   <td>千葉工大</td>
-                  <td>0 - 13</td>
+                  <td>0 - <b>13</b></td>
                   <td>京都大</td>
                 </tr>
                 <tr>
                   <td>第5試合</td>
                   <td>奈良</td>
-                  <td>10 - 0</td>
+                  <td><b>10</b> - 0</td>
                   <td>学習院</td>
                 </tr>
                 <tr>
                   <td>第6試合</td>
                   <td>横国A</td>
-                  <td><a href={'/tournament/first/6'}>0 - 5</a></td>
+                  <td><a href={'/tournament/first/6'}>0 - <b>5</b></a></td>
                   <td>関西連合</td>
                 </tr>
                 <tr>
                   <td>第7試合</td>
                   <td>一橋B</td>
-                  <td>7 - 16</td>
+                  <td>7 - <b>16</b></td>
                   <td>北大</td>
                 </tr>
             
@@ -61,7 +61,7 @@ export default class TournamentDetail extends React.Component{
                   <td rowSpan={4} className='brown'>準々決勝</td>
                   <td>第1試合</td>
                   <td>明治</td>
-                  <td>3 - 0</td>
+                  <td><b>3</b> - 0</td>
                   <td>横国B</td>
                 </tr>
                 <tr>
@@ -73,13 +73,13 @@ export default class TournamentDetail extends React.Component{
                 <tr>
                   <td>第3試合</td>
                   <td>奈良</td>
-                  <td>2 - 1</td>
+                  <td><b>2</b> - 1</td>
                   <td>関西連合</td>
                 </tr>
                 <tr>
                   <td>第4試合</td>
                   <td>北大</td>
-                  <td>6 - 1</td>
+                  <td><b>6</b> - 1</td>
                   <td>一橋A</td>
                 </tr>
               
@@ -87,7 +87,7 @@ export default class TournamentDetail extends React.Component{
                   <td rowSpan={2} className='purple'>準決勝</td>
                   <td>第1試合</td>
                   <td>明治</td>
-                  <td>7 - 3</td>
+                  <td><b>7</b> - 3</td>
                   <td>京大</td>
                 </tr>
                 <tr>
